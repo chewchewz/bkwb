99ee273b52d0dc1684607a70721a2ac1374f48ca
Iain Walmsley <iain.walmsley@gmail.com>
2018-05-29 16:18:49 +0000

    Adding more bootstrap styling

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.web_console.whitelisted_ips = '10.0.2.2'
+     config.web_console.whitelisted_ips = '10.0.2.2'
   end
 
   def self.setup_console
